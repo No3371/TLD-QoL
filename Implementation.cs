e2b2e950b2f38363729d61adf5c9ab28a8d3ca88
BA <developer@3371.online>
2023-05-18 13:42:43 +0800

    Action Picker W/S

--- Implementation.cs
+++ Implementation.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using HarmonyLib;
 using Il2Cpp;
 using MelonLoader;
@@ -166,6 +166,39 @@ namespace QoL
 		}
 	}
 
+	[HarmonyPatch(typeof(Panel_ActionPicker), nameof(Panel_ActionPicker.EnableWithCurrentList))]
+	internal class AcitonPickerMonitor
+	{
+		private static void Postfix(Panel_ActionPicker __instance)
+		{
+			__instance.NextItem();
+			__instance.PreviousItem();
+		}
+	}
+
+	[HarmonyPatch(typeof(Panel_ActionPicker), nameof(Panel_ActionPicker.Update))]
+	internal class AcitonPickerADAndConfirmation
+	{
+		// internal static int index;
+		private static void Postfix(Panel_ActionPicker __instance)
+		{
+			if (__instance.m_GenericProgressBar.IsEnabled()) return;
+			if (InputManager.GetKeyDown(InputManager.m_CurrentContext, Settings.options.interactKey))
+			{
+				__instance.OnSelect(__instance.m_SelectedIndex);
+				return;
+			}
+            if (InputManager.GetKeyDown(InputManager.m_CurrentContext, KeyCode.A) || InputManager.GetScroll(InputManager.m_CurrentContext) > 0)
+			{
+				__instance.PreviousItem();
+			}
+            else if (InputManager.GetKeyDown(InputManager.m_CurrentContext, KeyCode.D) || InputManager.GetScroll(InputManager.m_CurrentContext) < 0)
+			{
+				__instance.NextItem();
+			}
+		}
+	}
+
 	[HarmonyPatch(typeof(Panel_BodyHarvest), nameof(Panel_BodyHarvest.Update))]
 	internal class BodyHarvestToolScrollAndAlternativeHarvest
 	{
@@ -223,11 +256,22 @@ namespace QoL
 		}
 	}
 
+	[HarmonyPatch(typeof(Panel_FeedFire), nameof(Panel_FeedFire.Enable))]
+	internal class FeedFireUITracker
+	{
+		internal static int lastEntered;
+		private static void Postfix(ref Panel_FeedFire __instance)
+		{
+			lastEntered = Time.frameCount;
+		}
+	}
+
 	[HarmonyPatch(typeof(Panel_FeedFire), nameof(Panel_FeedFire.Update))]
 	internal class AlternativeFeedFire
 	{
 		private static void Postfix(ref Panel_FeedFire __instance)
 		{
+			if (__instance.IsDoingAction() || Time.frameCount - FeedFireUITracker.lastEntered < 30) return;
 			if (InputManager.GetKeyDown(InputManager.m_CurrentContext, Settings.options.interactKey))
 			{
 				__instance.OnFeedFire();
