8fa14278ba0f9d7fb91bc9b0c4e52276db9969a0
BA <developer@3371.online>
2023-05-29 22:52:14 +0800

    Void Trash Cans

--- Implementation.cs
+++ Implementation.cs
@@ -1221,8 +1221,49 @@ namespace QoL
 			if (Mathf.Approximately(lodScale, 1)) return;
 			if (__instance.GetComponent<GearItem>() == null && __instance.TryGetComponent<LODGroup>(out var g))
             {
-                g.size *= lodScale;
-            }
+
+	[HarmonyPatch(typeof(Panel_Container), nameof(Panel_Container.OnDone))]
+	internal class TrashCanWarning
+	{
+		static void Postfix (Panel_Container __instance)
+		{
+			if (Settings.options.voidTrashCan
+			 && __instance.m_Container.name == "CONTAINER_TrashCanister"
+			 && !__instance.m_Container.IsEmpty())
+			{
+				MelonCoroutines.Start(DelayedWarning());
+			}
+        }
+
+		static IEnumerator DelayedWarning ()
+		{
+			yield return new WaitForSeconds(0.5f);
+			InterfaceManager.GetPanel<Panel_HUD>().DisplayWarningMessage("VOID TRASH CAN");
+			yield return new WaitForSeconds(4f);
+			InterfaceManager.GetPanel<Panel_HUD>().ClearWarningMessage();
+		}
+
+	}
+
+	[HarmonyPatch(typeof(Container), nameof(Container.UpdateContainer))]
+	internal class TrashCan
+	{
+		static Il2CppSystem.Collections.Generic.List<GearItem> cache = new Il2CppSystem.Collections.Generic.List<GearItem>();
+		static void Postfix (Container __instance)
+		{
+			if (!__instance.m_StartHasBeenCalled || !Settings.options.voidTrashCan) return;
+			if (!__instance.IsInspected() || __instance.name != "CONTAINER_TrashCanister" || __instance.IsEmpty()) return;
+
+			cache.Clear();
+			__instance.GetItems(cache);
+			foreach (var i in cache)
+			{
+				i.Degrade(i.GearItemData.MaxHP * 0.000005f);
+				if (i.GetNormalizedCondition() == 0)
+				{
+					__instance.DestroyGear(i);
+				}
+			}
         }
 	}
 }
