5c4c6a376beaa13c3ee5320e4ea03d8d1d9a3d52
BA <developer@3371.online>
2024-06-25 18:32:41 +0800

    1.6.3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.2")]
-[assembly: AssemblyFileVersion("1.6.2")]
-[assembly: MelonInfo(typeof(QoL.Implementation), "QoL", "1.6.2", "BA")]
+[assembly: AssemblyVersion("1.6.3")]
+[assembly: AssemblyFileVersion("1.6.3")]
+[assembly: MelonInfo(typeof(QoL.Implementation), "QoL", "1.6.3", "BA")]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
