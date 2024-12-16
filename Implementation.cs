328eb36f80b1eafd4c0b8b4f80efa6f01b50c940
BA <developer@3371.online>
2024-12-16 15:09:13 +0800

    1.6.6

--- Implementation.cs
+++ Implementation.cs
@@ -10,7 +10,7 @@ namespace QoL
 {
 	public class Implementation : MelonMod
 	{
-		public const string VERSION = "1.6.5";
+		public const string VERSION = "1.6.6";
         public override void OnInitializeMelon()
 		{
 			MelonLogger.Msg($"[{Info.Name}] Version {Info.Version} loaded!");
