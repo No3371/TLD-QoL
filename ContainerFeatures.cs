855ada280593bd1b59b7959e9ad0daf26bd7df5d
BA <developer@3371.online>
2024-12-27 23:10:19 +0800

    Moved: container slot limit

--- ContainerFeatures.cs
+++ ContainerFeatures.cs
@@ -17,35 +17,3 @@ internal class ContainerWeightLimitPatch
         __instance.m_Capacity = ItemWeight.FromKilograms(kg);
     }
 }
-
-[HarmonyPatch(typeof(Panel_Container), nameof(Panel_Container.OnInventoryToContainer))]
-internal class ContainerSlotLimitPatch
-{
-    private static bool Prefix(Panel_Container __instance)
-    {
-        if (Settings.options.containerSlotLimitRatio >= 0.1f && __instance.m_Container != null)
-        {
-            Container container = __instance.m_Container;
-            var slotLimit = Mathf.FloorToInt(container.m_Capacity.ToQuantity(1f) * Settings.options.containerSlotLimitRatio);
-            if (container.m_Items.Count >= slotLimit)
-            {
-                GameAudioManager.PlayGUIError();
-                return false;
-            }
-        }
-        return true;
-    }
-}
-[HarmonyPatch(typeof(Panel_Container), nameof(Panel_Container.RefreshContainerTable))]
-internal class ContainerLabelPatch
-{
-    private static void Postfix(Panel_Container __instance)
-    {
-        if (Settings.options.containerSlotLimitRatio < 0.1f
-         || __instance.m_Container == null) return;
-
-        Container container = __instance.m_Container;
-        var slotLimit = Mathf.FloorToInt(container.m_Capacity.ToQuantity(1f) * Settings.options.containerSlotLimitRatio);
-        __instance.m_Label_ContainerCapacity.text += $" ({__instance.m_Container.m_Items.Count}/{slotLimit})";
-    }
-}
--- Implementation.cs
+++ Implementation.cs
@@ -10,7 +10,7 @@ namespace QoL
 {
     public class Implementation : MelonMod
 	{
-		public const string VERSION = "1.7.0";
+		public const string VERSION = "1.7.1";
         public override void OnInitializeMelon()
 		{
 			MelonLogger.Msg($"[{Info.Name}] Version {Info.Version} loaded!");
